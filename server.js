f3f67e5666080ec8e1547177fe21be8e62a66464
Yulli Chong <yc@Yullis-MacBook-Pro.local>
2019-08-03 17:37:33 -0500

    hooking up to heroku

--- server.js
+++ server.js
@@ -9,19 +9,25 @@ const methodOverride = require('method-override')
 ///////     CONFIGURATIONS       ///////
 ////////////////////////////////////////
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI
 const Coupon = require('./models/coupon.js')
 /////// CONNECT TO MONGODB /////////////
-mongoose.connect('mongodb://localhost:27017/dcdb', {useNewUrlParser: true})
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
 mongoose.connection.once('open', () => {
   console.log('connected to mongo')
 })
+// Fix Depreciation Warnings from Mongoose*
+// May or may not need these depending on your Mongoose version
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 ////////////////////////////////////////
 ///////     MIDDLEWARE           ///////
 ////////////////////////////////////////
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'));
 app.use(express.static('public'))
+app.use(express.json());
 ///////// CONTROLLERS //////////////
 const couponController = require('./controllers/router.js')
 app.use(couponController);
